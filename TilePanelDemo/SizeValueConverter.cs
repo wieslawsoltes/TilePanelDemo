96a4cb9615f680115eb87c5fe61e73ce315c9933
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-15 14:41:21 +0200

    Create SizeValueConverter.cs

---
+++ TilePanelDemo/SizeValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace TilePanelDemo
+{
+    public class SizeValueConverter : IValueConverter
+    {
+        public static readonly SizeValueConverter Instance = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Size size)
+            {
+                return size.ToString();
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s)
+            {
+                return Size.Parse(s);
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+    }
+}
